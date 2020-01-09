ba99d3ba76854b0840431bec2919b6ec3d3c65d6
yranarf <yranarf@qq.com>
2020-01-09 18:56:36 +0800

    add script

---
+++ script/debian10_frrouting_install.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# YFsama Debian 10 OpenStack鏡像使用的安裝FRR脚本
+
+# 修改為清華鏡像源
+echo "
+deb https://mirrors.tuna.tsinghua.edu.cn/debian/ buster main contrib non-free
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/debian/ buster main contrib non-free
+deb https://mirrors.tuna.tsinghua.edu.cn/debian/ buster-updates main contrib non-free
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/debian/ buster-updates main contrib non-free
+deb https://mirrors.tuna.tsinghua.edu.cn/debian/ buster-backports main contrib non-free
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/debian/ buster-backports main contrib non-free
+deb https://mirrors.tuna.tsinghua.edu.cn/debian-security buster/updates main contrib non-free
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/debian-security buster/updates main contrib non-free
+" > /etc/apt/sources.list
+
+# 更改DNS
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
+
+# 更新系統 組件
+apt update -y
+apt upgrade -y
+apt install -y curl gnupg2 traceroute 
+
+# 安裝FRRouting
+
+curl -s https://deb.frrouting.org/frr/keys.asc | sudo apt-key add -
+FRRVER="frr-stable"
+echo deb https://deb.frrouting.org/frr $(lsb_release -s -c) $FRRVER | sudo tee -a /etc/apt/sources.list.d/frr.list
+sudo apt update -y && sudo apt install -y frr frr-pythontools
+
+# 開啓IP轉發
+echo "
+net.ipv4.conf.all.forwarding = 1
+net.ipv6.conf.all.disable_ipv6 = 0
+net.ipv6.conf.default.disable_ipv6 = 0
+net.ipv6.conf.lo.disable_ipv6 = 0
+net.ipv6.conf.default.forwarding = 1
+net.ipv6.conf.all.forwarding = 1
+net.ipv6.conf.all.proxy_ndp = 1
+net.ipv6.conf.all.accept_ra = 2
+" > /etc/sysctl.conf
+
+sysctl -p 
+
+# 打开Frr全部功能
+sed -i "s/=no/=yes/g" /etc/frr/daemons
+service frr restart
+
+sudo vtysh
