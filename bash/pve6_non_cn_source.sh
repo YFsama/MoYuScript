2be77a74acde175e5c0cb2ebd3fbe42fb82b7aa3
yranarf <yranarf@qq.com>
2020-01-14 14:15:59 +0800

    Update

---
+++ bash/pve6_non_cn_source.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "deb http://mirrors.ustc.edu.cn/proxmox/debian/pve buster pve-no-subscription " >> /etc/apt/sources.list.d/pve-no-sub.list
+
+# 注释掉企业源
+echo "#deb https://enterprise.proxmox.com/debian/pve buster pve-enterprise" > cat /etc/apt/sources.list.d/pve-enterprise.lis
---
+++ bash/pve_get_debian10_ci.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# YFsama PVE导入Debian10 鏡像使用
+
+apt install -y wget
+
+# 国内放个镜像
+wget "http://cdimage.debian.org/cdimage/cloud/OpenStack/current-10/debian-10-openstack-amd64.qcow2"
+
+qm importdisk $0 debian-10-openstack-amd64.qcow2 $1 
+qm set $0 --virtio0 $1:vm-$0-disk-0
+qm set $0 --boot c --bootdisk virtio0
+qm set $0 --serial0 socket --vga serial0
+qm set $0 --ide2 $1:cloudinit
+qm template $0
